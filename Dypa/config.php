e581c7186ffe0346863c97138921ad40599930be
athina-pap <57074771+athina-pap@users.noreply.github.com>
2020-05-27 23:11:48 +0300

    Update config.php

--- Dypa/config.php
+++ Dypa/config.php
@@ -1,6 +1,15 @@
-<?php 
+<?php
+$user='root';
+$pass='1998176Aa!';
+$host='';
+$db = 'museums';
+$turn1 = true;
+$turn2 = false;
 
-$con = mysqli_connect("localhost","root","");
-$db = mysqli_select_db($con,"museums");
-	
+
+$con = new mysqli($host, $user, $pass, $db,null,'/home/student/it/2016/it164725/mysql/run/mysql.sock');
+if ($con->connect_errno) {
+    echo "Failed to connect to MySQL: (" . 
+    $con->connect_errno . ") " . $mysqli->connect_error;
+
 ?>
